4189547a07fa32e30c036b9a671d189b939d9192
Are Almaas <arealmaas@gmail.com>
2012-05-27 13:11:33 +0200

    Adding the project

---
+++ ShoeAroundTheWorld/WebContent/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>A shoe around the world</title>
+	</head>
+	<body>
+		
+		
+		<div id="page">
+
+			<div id="top">
+				<div id="top_upper">
+					<div id="top_searchbar">
+					</div>
+					<div id="top_login">
+					</div>
+					
+				</div>
+				<div id="top_content">
+					<div id="top_banner">
+					</div>
+					<div id="top_menu">
+					</div>
+				
+				</div>
+
+
+			</div>
+		
+			<div id="content">
+
+
+
+
+
+			</div>
+
+			<div id="bottom">
+
+			
+			</div>
+
+		</div>
+		
+	</body>
+</html>
